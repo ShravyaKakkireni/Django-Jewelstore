d2aedaeb8671551317df7228e71c314e9f1c1acb
ShravyaKakkireni <92895062+ShravyaKakkireni@users.noreply.github.com>
2023-02-10 11:20:02 -0600

    changed smtp password

--- jewel/info.py
+++ jewel/info.py
@@ -1,7 +1,7 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'shavijewel@gmail.com'
-EMAIL_HOST_PASSWORD = 'rfcjewxdcgkcjxrp'
+EMAIL_HOST_PASSWORD = 'fecgkgnwcfpmdqpf'
 EMAIL_PORT = 587
 '''
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
